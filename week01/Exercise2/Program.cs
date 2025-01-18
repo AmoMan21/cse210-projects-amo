cc759cc9a7cd9ebf4ba05ddecc6b71636879f57b
AmoMan21 <oli24015@byui.edu>
2025-01-18 13:04:36 +0200

    Completed Exercise 2, Core Req. 1

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,39 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        // Ask the user for their grade percentage
+        Console.Write("Enter your grade percentage: ");
+        string input = Console.ReadLine();
+
+        // Convert the input to an integer
+        int gradePercentage = int.Parse(input);
+
+        // Initialize a variable for the letter grade
+        string letter;
+
+        // Determine the letter grade
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        // Dispay the letter grading
+        Console.WriteLine($"Your letter grade is: {letter}");
     }
 }
